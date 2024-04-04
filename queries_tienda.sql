aa7091736de93a39d3a66d0b4ad01b378c71172e
MaikGare <maik.fusrodah@gmail.com>
2024-04-04 12:24:57 +0200

    primera actualizacion queries_tienda

--- queries_tienda.sql
+++ queries_tienda.sql
@@ -3,5 +3,21 @@ SELECT nombre, precio FROM producto;
 SELECT * FROM producto;
 SELECT nombre, precio, precio * 1.08 FROM producto;
 SELECT nombre AS nombre_producto, precio AS euros, precio * 1.08 AS dólares FROM producto;
-SELECT upper(nombre), precio FROM producto;
-SELECT lower(nombre), precio FROM producto;
+SELECT UPPER(nombre), precio FROM producto;
+SELECT LOWER(nombre), precio FROM producto;
+SELECT nombre, UPPER(SUBSTRING(nombre,1,2)) FROM fabricante;
+SELECT nombre, ROUND(precio,0) FROM producto;
+SELECT nombre, TRUNCATE(precio,0) FROM producto;
+SELECT codigo_fabricante FROM producto;
+SELECT DISTINCT codigo_fabricante FROM producto;
+SELECT nombre FROM fabricante ORDER BY nombre ASC;
+SELECT nombre FROM fabricante ORDER BY nombre DESC;
+SELECT nombre FROM producto ORDER BY nombre ASC, precio DESC;
+SELECT * FROM fabricante LIMIT 5;
+SELECT * FROM fabricante LIMIT 3,2;
+SELECT nombre, precio FROM producto ORDER BY precio ASC LIMIT 1;
+SELECT nombre, precio FROM producto ORDER BY precio DESC LIMIT 1;
+SELECT nombre FROM producto WHERE codigo_fabricante = 2;
+
+
+
